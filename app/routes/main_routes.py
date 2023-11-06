8718d016320cbf683ceae1be194c19853a37c8a6
val832 <valentin.naud@etudiant.univ-reims.fr>
2023-11-06 10:12:12 +0100

    import du projet

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask import jsonify
-from sqlalchemy import func
+from sqlalchemy import func, text
 from app.models.models import Product # Assurez-vous que cet import est correct en fonction de la structure de votre dossier
 from app import db # Assurez-vous que cet import est correct en fonction de la structure de votre dossier
 
@@ -20,10 +20,34 @@ def get_purchasable_products_count():
 
 @main_bp.route('/products-per-category', methods=['GET'])
 def products_per_category():
-    category_counts = db.session.query(Product.PrimaryCategoryName, 
-                                       func.count(Product.ProductID)).group_by(Product.PrimaryCategoryName).all()
-    
-    result = {category: count for category, count in category_counts}
-    
-    return jsonify(result)
 
+    sql_query = text("select primarycategoryname, count(productid) from product group by primarycategoryname ;")
+    #category_counts = db.session.query(Product.PrimaryCategoryName, 
+                                       #func.count(Product.ProductID)).group_by(Product.PrimaryCategoryName).all()
+    
+    #result = {category: count for category, count in category_counts}
+    
+    result = db.session.execute(sql_query)
+    categories = result.fetchall()
+    
+    print = {category[0]: category[1] for category in categories}
+    
+    return jsonify(print)
+
+@main_bp.route('/products/<string:ean>', methods=['GET'])
+def get_product_details(ean):
+    # construction de la requête sql
+    sql_query = text("SELECT * FROM product WHERE ean = :ean;")
+    
+
+    # On éxécute la requête sql et on récupère son contenu 
+    result = db.session.execute(sql_query, {'ean':ean})
+    product = result.fetchone()
+
+    #On vérifie que le produit soit trouvé 
+    if product:
+        # On convertit en dictionnaire 
+        product_data = product._asdict()
+        return jsonify(product_data), 200
+    else:
+        return jsonify({"error": "Product not found"}), 404
